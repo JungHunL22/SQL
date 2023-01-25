5f4911b37a5f93aac0230a5b4b2ca8a1389768e5
Lee Jung Hun <108857910+JungHunL22@users.noreply.github.com>
2023-01-25 17:04:51 +0900

    Update 오프라인／온라인 판매 데이터 통합하기.sql

--- 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -3,11 +3,3 @@ SELECT date_format(sales_date,'%Y-%m-%d') sales_date,product_id,user_id,sales_am
 where month(sales_date)=3 union all select sales_date,product_id,null as user_id, sales_amount 
 from offline_sale where month(sales_date)=3
 order by sales_date,product_id,user_id
-                   
-# SELECT DATE_FORMAT(SALES_DATE, '%Y-%m-%d') AS SALES_DATE, PRODUCT_ID,USER_ID,SALES_AMOUNT  FROM ONLINE_SALE
-                  
-# WHERE SALES_DATE BETWEEN '2022-03-01' AND '2022-03-31'
-# UNION ALL
-# SELECT SALES_DATE, PRODUCT_ID, NULL AS USER_ID, SALES_AMOUNT  FROM OFFLINE_SALE
-# WHERE SALES_DATE BETWEEN '2022-03-01' AND '2022-03-31'
-# ORDER BY SALES_DATE ASC, PRODUCT_ID ASC, USER_ID ASC;
