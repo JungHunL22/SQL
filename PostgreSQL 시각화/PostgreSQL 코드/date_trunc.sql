5fa149edd3c7cd2d7b90ab8129ddc5c8c799e3c0
lee <duddlswnddb@naver.com>
2023-05-11 23:22:04 +0900

    date_trunc활용 일/월별 합계

---
+++ PostgreSQL 시각화/PostgreSQL 코드/date_trunc.sql
@@ -0,0 +1,23 @@
+/*
+*************************************
+일/주/월/분기별 매출 및 주문건수 실적
+*************************************
+*/
+/* 학습함수 : date_trunc
+ * date_trunc(날짜형식,필드명) : 날짜필드를 입력한 날짜형식 기준으로 인수를 사용하여 다양한 날짜, 시간 형식으로 데이터를 자를 
+ * ::date를 넣어주면 출력값 디폴트가 타임스탬프에서 날짜 형식으로 변환되어 출력됨 
+ * */
+-- 일별 매출 및 주문건수
+select date_trunc('day', order_date)::date as day
+	, sum(amount) as sum_amount, count(distinct a.order_id) as daily_ord_cnt
+from nw.orders a
+	join nw.order_items b on a.order_id = b.order_id
+group by date_trunc('day', order_date)::date 
+order by 1;
+
+-- 월별 매출 및 주문건수
+select date_trunc('month',order_date)::date as month_day,
+sum(amount) as sum_amount,count(distinct a.order_id) as month_ord_cnt
+from nw.orders a
+join nw.order_items b on a.order_id=b.order_id
+group by date_trunc('month',order_date)::date order by 1;
